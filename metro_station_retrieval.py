3d83046629e1d3ddeb4d534e1c7ac03b5a6b1cbe
vangarcciaa <vgarcia3@usc.edu>
2025-11-24 22:47:56 -0800

    Updated the code on 11/24 to include final code.

--- metro_station_retrieval.py
+++ metro_station_retrieval.py
@@ -1,25 +1,27 @@
 pip install geopandas
 import geopandas as gpd
-import pandas as pd
 
-#????? DO I INCLUDE THIS HERE ??????
-#path to the shapefile
+# path to the shapefile
 shp_path = "230711_All_MetroRail_Stations.shp"
+
 gdf = gpd.read_file(shp_path)
 
-#checking to see if it was successful 
+# checking to see if it was successful 
 print(gdf.head())
 print(gdf.columns)
 
-#converting to pandas DataFrame
+# converting to pandas DataFrame
+import pandas as pd
+
 df = pd.DataFrame(gdf.drop(columns="geometry"))
 print(df.head())
 
-#this file comes with latitude and longitude data
-#using geometry function to store each station location as a point (longitude, latitude)
-#(LAT,LONG) is the format for GIS shapefiles
+# this file comes with latitude and longitude data
+# using geometry function to store each station location as a point (longitude, latitude)
+# (LAT,LONG) is the format for GIS shapefiles
+
 gdf["lon"] = gdf.geometry.x
 gdf["lat"] = gdf.geometry.y
 
-#saving to csv file 
+# saving to csv file 
 gdf.to_csv("metro_stations.csv", index=False)
